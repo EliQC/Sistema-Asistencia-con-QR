ed971583c226dffe771250c317fd5885c1e70aad
ElíQC <75975537@elp.edu.pe>
2025-11-12 12:32:43 -0500

    Feat: Agrega script de construcción build.sh para despliegue > > Este script automatiza los pasos necesarios para preparar la > aplicación en un entorno de producción: > > - Instala las dependencias de Python. > - Recolecta los archivos estáticos. > - Aplica las migraciones de la base de datos. > - Crea un superusuario a partir de variables de entorno si no existe. > > Esencial para plataformas de hosting como Render.

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# Instalar dependencias
+pip install -r requirements.txt
+
+
+# Aplicar migraciones a la base de datos
+python manage.py migrate
+
+# Crear un superusuario si no existe (usando variables de entorno)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); import os; username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); email=os.environ.get('DJANGO_SUPERUSER_EMAIL'); password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)" | python manage.py shell
